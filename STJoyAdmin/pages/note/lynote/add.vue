1eaf86a86e5e4ed531c99e5bf14fb96ec925cd0c
shenzhaoliang <409178030@qq.com>
2024-01-25 15:26:52 +0800

    uni

---
+++ STJoyAdmin/pages/note/lynote/add.vue
@@ -0,0 +1,105 @@
+<template>
+  <view class="uni-container">
+    <uni-forms ref="form" :model="formData" validateTrigger="bind">
+      <uni-forms-item name="create_time" label="">
+        <uni-datetime-picker return-type="timestamp" v-model="formData.create_time"></uni-datetime-picker>
+      </uni-forms-item>
+      <uni-forms-item name="update_time" label="">
+        <uni-datetime-picker return-type="timestamp" v-model="formData.update_time"></uni-datetime-picker>
+      </uni-forms-item>
+      <uni-forms-item name="user_id" label="">
+        <uni-easyinput placeholder="用户id" v-model="formData.user_id"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="title" label="标题">
+        <uni-easyinput placeholder="标题" v-model="formData.title" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="content" label="文章内容">
+        <uni-easyinput placeholder="文章内容" v-model="formData.content" trim="right"></uni-easyinput>
+      </uni-forms-item>
+      <view class="uni-button-group">
+        <button type="primary" class="uni-button" style="width: 100px;" @click="submit">提交</button>
+        <navigator open-type="navigateBack" style="margin-left: 15px;">
+          <button class="uni-button" style="width: 100px;">返回</button>
+        </navigator>
+      </view>
+    </uni-forms>
+  </view>
+</template>
+
+<script>
+  import { validator } from '@/js_sdk/validator/ly-note.js';
+
+  const db = uniCloud.database();
+  const dbCmd = db.command;
+  const dbCollectionName = 'ly-note';
+
+  function getValidator(fields) {
+    let result = {}
+    for (let key in validator) {
+      if (fields.includes(key)) {
+        result[key] = validator[key]
+      }
+    }
+    return result
+  }
+
+  
+
+  export default {
+    data() {
+      let formData = {
+        "create_time": null,
+        "update_time": null,
+        "user_id": "",
+        "title": "",
+        "content": ""
+      }
+      return {
+        formData,
+        formOptions: {},
+        rules: {
+          ...getValidator(Object.keys(formData))
+        }
+      }
+    },
+    onReady() {
+      this.$refs.form.setRules(this.rules)
+    },
+    methods: {
+      
+      /**
+       * 验证表单并提交
+       */
+      submit() {
+        uni.showLoading({
+          mask: true
+        })
+        this.$refs.form.validate().then((res) => {
+          return this.submitForm(res)
+        }).catch(() => {
+        }).finally(() => {
+          uni.hideLoading()
+        })
+      },
+
+      /**
+       * 提交表单
+       */
+      submitForm(value) {
+        // 使用 clientDB 提交数据
+        return db.collection(dbCollectionName).add(value).then((res) => {
+          uni.showToast({
+            title: '新增成功'
+          })
+          this.getOpenerEventChannel().emit('refreshData')
+          setTimeout(() => uni.navigateBack(), 500)
+        }).catch((err) => {
+          uni.showModal({
+            content: err.message || '请求服务失败',
+            showCancel: false
+          })
+        })
+      }
+    }
+  }
+</script>
---
+++ STJoyAdmin/pages/note/lynote/edit.vue
@@ -0,0 +1,136 @@
+<template>
+  <view class="uni-container">
+    <uni-forms ref="form" :model="formData" validateTrigger="bind">
+      <uni-forms-item name="create_time" label="">
+        <uni-datetime-picker return-type="timestamp" v-model="formData.create_time"></uni-datetime-picker>
+      </uni-forms-item>
+      <uni-forms-item name="update_time" label="">
+        <uni-datetime-picker return-type="timestamp" v-model="formData.update_time"></uni-datetime-picker>
+      </uni-forms-item>
+      <uni-forms-item name="user_id" label="">
+        <uni-easyinput placeholder="用户id" v-model="formData.user_id"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="title" label="标题">
+        <uni-easyinput placeholder="标题" v-model="formData.title" trim="both"></uni-easyinput>
+      </uni-forms-item>
+      <uni-forms-item name="content" label="文章内容">
+        <uni-easyinput placeholder="文章内容" v-model="formData.content" trim="right"></uni-easyinput>
+      </uni-forms-item>
+      <view class="uni-button-group">
+        <button type="primary" class="uni-button" style="width: 100px;" @click="submit">提交</button>
+        <navigator open-type="navigateBack" style="margin-left: 15px;">
+          <button class="uni-button" style="width: 100px;">返回</button>
+        </navigator>
+      </view>
+    </uni-forms>
+  </view>
+</template>
+
+<script>
+  import { validator } from '@/js_sdk/validator/ly-note.js';
+
+  const db = uniCloud.database();
+  const dbCmd = db.command;
+  const dbCollectionName = 'ly-note';
+
+  function getValidator(fields) {
+    let result = {}
+    for (let key in validator) {
+      if (fields.includes(key)) {
+        result[key] = validator[key]
+      }
+    }
+    return result
+  }
+
+  
+
+  export default {
+    data() {
+      let formData = {
+        "create_time": null,
+        "update_time": null,
+        "user_id": "",
+        "title": "",
+        "content": ""
+      }
+      return {
+        formData,
+        formOptions: {},
+        rules: {
+          ...getValidator(Object.keys(formData))
+        }
+      }
+    },
+    onLoad(e) {
+      if (e.id) {
+        const id = e.id
+        this.formDataId = id
+        this.getDetail(id)
+      }
+    },
+    onReady() {
+      this.$refs.form.setRules(this.rules)
+    },
+    methods: {
+      
+      /**
+       * 验证表单并提交
+       */
+      submit() {
+        uni.showLoading({
+          mask: true
+        })
+        this.$refs.form.validate().then((res) => {
+          return this.submitForm(res)
+        }).catch(() => {
+        }).finally(() => {
+          uni.hideLoading()
+        })
+      },
+
+      /**
+       * 提交表单
+       */
+      submitForm(value) {
+        // 使用 clientDB 提交数据
+        return db.collection(dbCollectionName).doc(this.formDataId).update(value).then((res) => {
+          uni.showToast({
+            title: '修改成功'
+          })
+          this.getOpenerEventChannel().emit('refreshData')
+          setTimeout(() => uni.navigateBack(), 500)
+        }).catch((err) => {
+          uni.showModal({
+            content: err.message || '请求服务失败',
+            showCancel: false
+          })
+        })
+      },
+
+      /**
+       * 获取表单数据
+       * @param {Object} id
+       */
+      getDetail(id) {
+        uni.showLoading({
+          mask: true
+        })
+        db.collection(dbCollectionName).doc(id).field("create_time,update_time,user_id,title,content").get().then((res) => {
+          const data = res.result.data[0]
+          if (data) {
+            this.formData = data
+            
+          }
+        }).catch((err) => {
+          uni.showModal({
+            content: err.message || '请求服务失败',
+            showCancel: false
+          })
+        }).finally(() => {
+          uni.hideLoading()
+        })
+      }
+    }
+  }
+</script>
