6e87e48fe15df1bd9914f243bd2230ce992d297a
shenzhaoliang <409178030@qq.com>
2024-01-25 17:07:42 +0800

    add

--- STJoyAdmin/pages/note/lynote/add.vue
+++ STJoyAdmin/pages/note/lynote/add.vue
@@ -1,92 +1,78 @@
 <template>
-  <view class="uni-container">
-    <uni-forms ref="form" :model="formData" validateTrigger="bind">
-      <uni-forms-item name="create_time" label="">
-        <uni-datetime-picker return-type="timestamp" v-model="formData.create_time"></uni-datetime-picker>
-      </uni-forms-item>
-      <uni-forms-item name="update_time" label="">
-        <uni-datetime-picker return-type="timestamp" v-model="formData.update_time"></uni-datetime-picker>
-      </uni-forms-item>
-      <uni-forms-item name="user_id" label="">
-        <uni-easyinput placeholder="用户id" v-model="formData.user_id"></uni-easyinput>
-      </uni-forms-item>
-      <uni-forms-item name="title" label="标题">
-        <uni-easyinput placeholder="标题" v-model="formData.title" trim="both"></uni-easyinput>
-      </uni-forms-item>
-      <uni-forms-item name="content" label="文章内容">
-        <uni-easyinput placeholder="文章内容" v-model="formData.content" trim="right"></uni-easyinput>
-      </uni-forms-item>
-      <view class="uni-button-group">
-        <button type="primary" class="uni-button" style="width: 100px;" @click="submit">提交</button>
-        <navigator open-type="navigateBack" style="margin-left: 15px;">
-          <button class="uni-button" style="width: 100px;">返回</button>
-        </navigator>
-      </view>
-    </uni-forms>
-  </view>
+	<view class="uni-container">
+		<uni-forms ref="form" :model="formData" validateTrigger="bind">
+			<uni-forms-item name="title" label="标题">
+				<uni-easyinput placeholder="标题" v-model="formData.title" trim="both"></uni-easyinput>
+			</uni-forms-item>
+			<uni-forms-item name="content" label="文章内容">
+				<uni-easyinput placeholder="文章内容" v-model="formData.content" trim="right"></uni-easyinput>
+			</uni-forms-item>
+			<view class="uni-button-group">
+				<button type="primary" class="uni-button" style="width: 100px;" @click="submit">提交</button>
+				<navigator open-type="navigateBack" style="margin-left: 15px;">
+					<button class="uni-button" style="width: 100px;">返回</button>
+				</navigator>
+			</view>
+		</uni-forms>
+	</view>
 </template>
 
 <script>
-  import { validator } from '@/js_sdk/validator/ly-note.js';
-  function getValidator(fields) {
-    let result = {}
-    for (let key in validator) {
-      if (fields.includes(key)) {
-        result[key] = validator[key]
-      }
-    }
-    return result
-  }
-  export default {
-    data() {
-      let formData = {
-        "create_time": null,
-        "update_time": null,
-        "user_id": "",
-        "title": "",
-        "content": ""
-      }
-      return {
-        formData,
-        formOptions: {},
-        rules: {
-          ...getValidator(Object.keys(formData))
-        }
-      }
-    },
-    onReady() {
-      this.$refs.form.setRules(this.rules)
-    },
-    methods: {
-      
-      /**
-       * 验证表单并提交
-       */
-      submit() {
-        uni.showLoading({
-          mask: true
-        })
-        this.$refs.form.validate().then((res) => {
-          return this.submitForm(res)
-        }).catch(() => {
-        }).finally(() => {
-          uni.hideLoading()
-        })
-      },
-      submitForm(value) {
+	import {
+		validator
+	} from '@/js_sdk/validator/ly-note.js';
+
+	function getValidator(fields) {
+		let result = {}
+		for (let key in validator) {
+			if (fields.includes(key)) {
+				result[key] = validator[key]
+			}
+		}
+		return result
+	}
+	export default {
+		data() {
+			let formData = {
+				"title": "",
+				"content": ""
+			}
+			return {
+				formData,
+				formOptions: {},
+				rules: {
+					...getValidator(Object.keys(formData))
+				}
+			}
+		},
+		onReady() {
+			this.$refs.form.setRules(this.rules)
+		},
+		methods: {
+			submit() {
+				uni.showLoading({
+					mask: true
+				})
+				this.$refs.form.validate().then((res) => {
+					return this.submitForm(res)
+				}).finally(() => {
+					uni.hideLoading()
+				})
+			},
+			submitForm(value) {
 				uniCloud.importObject("ylnote").add(value).then((res) => {
-          uni.showToast({
-            title: '新增成功'
-          })
-          this.getOpenerEventChannel().emit('refreshData')
-          setTimeout(() => uni.navigateBack(), 500)
-        }).catch((err) => {
-          uni.showModal({
-            content: err.message || '请求服务失败',
-            showCancel: false
-          })
-        })
-      }
-    }
-  }
+					uni.showToast({
+						title: '新增成功'
+					})
+					this.getOpenerEventChannel().emit('refreshData')
+					setTimeout(() => uni.navigateBack(), 500)
+				}).catch((err) => {
+					uni.showModal({
+						content: err.message || '请求服务失败',
+						showCancel: false
+					})
+				})
+			}
+		}
+	}
 </script>
--- STJoyAdmin/uniCloud-aliyun/cloudfunctions/ylnote/index.obj.js
+++ STJoyAdmin/uniCloud-aliyun/cloudfunctions/ylnote/index.obj.js
@@ -1,13 +1,28 @@
 const db = uniCloud.database();
 const dbCmd = db.command;
 const dbCollectionName = 'ly-note';
+const uniID = require('uni-id-common')
 module.exports = {
-	_before: function() { // 通用预处理器
-
+	_before: async function() { // 通用预处理器
+		this.token = this.getUniIdToken()
+		if (this.token) {
+			const clientInfo = this.getClientInfo()
+			this.uniID = uniID.createInstance({ // 创建uni-id实例，其上方法同uniID
+				clientInfo
+			})
+			this.userInfo = await this.uniID.checkToken(this.token);
+		} else {
+			throw new Error('token凭证不存在，请重新登录')
+		}
 	},
 	async add(value) {
+		if (this.userInfo.errCode) {
+			return this.userInfo
+		}
+		value.user_id = this.userInfo.uid
+		value.create_time = Date.now()
+		value.update_time = Date.now()
 		const result = await db.collection(dbCollectionName).add(value)
-		console.log("000", result)
 		return result
 	}
 }
